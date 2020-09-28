6946c13f7dd7740c09e3585439fda6c5ab7a923a
hubo7 <hubo7@le.com>
2020-09-28 10:58:50 +0800

    init

---
+++ src/main/java/com/letv/HelloJava.java
@@ -0,0 +1,12 @@
+package com.letv;
+
+/**
+ * @Description
+ * @Author Allen
+ * @Date 2020-09-27 17:00
+ **/
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/scala/com/letv/App.scala
@@ -0,0 +1,8 @@
+package com.letv
+
+/**
+ * com.letv.Hello world!
+ */
+object Hello extends App {
+  println( "Hello World!" )
+}
